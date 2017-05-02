fc184b43d162a55b1192e7741e1525eb8c2f8ae1
Rob Court <robbie.court@gmail.com>
2017-05-02 13:44:43 +0100

    Update startNBLAST.sh

--- startNBLAST.sh
+++ startNBLAST.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 cd /
-wget http://data.virtualflybrain.org/archive/site-library.tar.gz
-wget http://data.virtualflybrain.org/archive/nblast.tar.gz
-tar -xzf site-library.tar.gz
-tar -xzf nblast.tar.gz
-rm site-library.tar.gz
-rm nblast.tar.gz
+# wget http://data.virtualflybrain.org/archive/site-library.tar.gz
+# wget http://data.virtualflybrain.org/archive/nblast.tar.gz
+# tar -xzf site-library.tar.gz
+# tar -xzf nblast.tar.gz
+# rm site-library.tar.gz
+# rm nblast.tar.gz
 
 service opencpu restart
 

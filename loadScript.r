c34d414167491097327ff4f82028b2e351482441
Rob Court <robbie.court@gmail.com>
2017-05-03 13:34:58 +0100

    Create loadScript.r

---
+++ loadScript.r
@@ -0,0 +1,8 @@
+install.packages("memoise", repos="https://cloud.r-project.org/")
+install.packages("jsonlite", repos="https://cloud.r-project.org/")
+install.packages("devtools", repos="https://cloud.r-project.org/")
+install.packages("rgl", repos="https://cloud.r-project.org/")
+devtools::install_github("jefferis/nat", dependencies=TRUE)
+devtools::install_github("jefferis/flycircuit", dependencies=TRUE)
+devtools::install_github("jefferis/flynblastscores", dependencies=TRUE)
+devtools::install_github("jefferis/vfbr", dependencies=TRUE)

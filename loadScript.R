24129129a5611925867b6120edadafab5a5b1115
Rob Court <robbie.court@gmail.com>
2017-05-04 18:24:37 +0100

    explicitly setting

--- loadScript.R
+++ loadScript.R
@@ -1,5 +1,7 @@
 #!/usr/bin/env Rscript
 
+options(flycircuit.datadir="/data")
+options(rgl.useNULL=TRUE)
 install.packages("memoise", repos="https://cloud.r-project.org/")
 install.packages("jsonlite", repos="https://cloud.r-project.org/")
 install.packages("devtools", repos="https://cloud.r-project.org/")

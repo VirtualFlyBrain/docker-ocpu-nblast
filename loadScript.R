6805b615537801b0e19ae4207a9f8b4fc75b3fc3
Rob Court <robbie.court@gmail.com>
2017-05-05 06:08:02 +0100

    forcing to use opencpu lib

--- loadScript.R
+++ loadScript.R
@@ -1,5 +1,5 @@
 #!/usr/bin/env Rscript
-
+lib="/usr/lib/opencpu/library"
 options(flycircuit.datadir="/data")
 options(rgl.useNULL=TRUE)
 install.packages("brew", repos="https://cloud.r-project.org/", dependencies = TRUE)

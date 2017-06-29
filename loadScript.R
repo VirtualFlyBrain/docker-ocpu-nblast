c671182e155aa5f389dbe120231924bcefcfe602
Rob Court <robbie.court@gmail.com>
2017-06-29 12:17:20 +0100

    forcing install
    sever require compilation

--- loadScript.R
+++ loadScript.R
@@ -1,24 +1,15 @@
 #!/usr/bin/env Rscript
 
-pkgTest <- function(x)
-  {
-    if (!require(x,character.only = TRUE))
-    {
-      install.packages(x,repos="https://cloud.r-project.org/", dependencies = TRUE)
-        if(!require(x,character.only = TRUE)) stop("Package not found")
-    }
-  }
-
 options(flycircuit.datadir="/data")
 options(rgl.useNULL=TRUE)
-pkgTest("igraph")
-pkgTest("brew")
-pkgTest("git2r")
-pkgTest("memoise")
-pkgTest("jsonlite")
-pkgTest("devtools")
-pkgTest("ggplot2")
-pkgTest("rgl")
+install.packages("igraph",repos="https://cloud.r-project.org/", dependencies = TRUE)
+install.packages("brew",repos="https://cloud.r-project.org/", dependencies = TRUE)
+install.packages("git2r",repos="https://cloud.r-project.org/", dependencies = TRUE)
+install.packages("memoise",repos="https://cloud.r-project.org/", dependencies = TRUE)
+install.packages("jsonlite",repos="https://cloud.r-project.org/", dependencies = TRUE)
+install.packages("devtools",repos="https://cloud.r-project.org/", dependencies = TRUE)
+install.packages("ggplot2",repos="https://cloud.r-project.org/", dependencies = TRUE)
+install.packages("rgl",repos="https://cloud.r-project.org/", dependencies = TRUE)
 devtools::install_github("jefferis/nat", dependencies=TRUE)
 devtools::install_github("jefferis/flycircuit", dependencies=TRUE)
 devtools::install_github("jefferis/flynblastscores", dependencies=TRUE)

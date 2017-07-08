bbd0506494782751be35741239df6197d3bf076d
Rob Court <robbie.court@gmail.com>
2017-07-08 13:23:01 +0100

    devtools

--- loadScript.R
+++ loadScript.R
@@ -3,6 +3,7 @@
 options(flycircuit.datadir="/data")
 options(rgl.useNULL=TRUE)
 options(repos=structure(c(CRAN="https://cloud.r-project.org/")))
+install.packages("devtools",repos="https://cloud.r-project.org/", dependencies = TRUE)
 devtools::install_github("jefferis/nat", force=TRUE, dependencies=TRUE)
 devtools::install_github("jefferis/flycircuit", force=TRUE, dependencies=TRUE)
 devtools::install_github("jefferis/flynblastscores", force=TRUE, dependencies=TRUE)

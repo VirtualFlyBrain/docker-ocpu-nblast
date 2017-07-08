5f7c02946960d6bbafc1a3580ed8209768ce37eb
Rob Court <robbie.court@gmail.com>
2017-07-08 19:31:28 +0100

    Update loadScript.R

--- loadScript.R
+++ loadScript.R
@@ -12,6 +12,6 @@ install.packages("devtools",repos="https://cloud.r-project.org/", dependencies =
 install.packages("ggplot2",repos="https://cloud.r-project.org/", dependencies = TRUE)
 install.packages("rgl",repos="https://cloud.r-project.org/", dependencies = TRUE)
 devtools::install_github("jefferis/nat", force=TRUE, dependencies=TRUE)
-devtools::install_github("jefferis/flycircuit", force=TRUE, dependencies=TRUE)
 devtools::install_github("jefferis/vfbr", force=TRUE, dependencies=TRUE)
+devtools::install_github("jefferis/flycircuit", force=TRUE, dependencies=TRUE)
 devtools::install_github("jefferis/flynblastscores", force=TRUE, dependencies=TRUE)

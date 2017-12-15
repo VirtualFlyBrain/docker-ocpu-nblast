1c4e320ac3315388e5ee55fad80663b5cdca0914
Rob Court <robbie.court@gmail.com>
2017-12-15 18:13:32 +0000

    checking before install

--- loadScript.R
+++ loadScript.R
@@ -3,7 +3,7 @@
 .libPaths()
 options(flycircuit.datadir="/data")
 options(rgl.useNULL=TRUE)
-install.packages("jsonlite",repos="https://cloud.r-project.org/", dependencies = TRUE)
+if (!require("jsonlite")) install.packages("jsonlite",repos="https://cloud.r-project.org/", dependencies = TRUE))
 devtools::install_github("jefferis/nat", force=TRUE, dependencies=TRUE)
 devtools::install_github("jefferis/vfbr", force=TRUE, dependencies=TRUE)
 devtools::install_github("jefferis/flycircuit", force=TRUE, dependencies=TRUE)

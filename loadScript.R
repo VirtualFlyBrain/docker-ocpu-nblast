675e808ccce531317c3395041a4f1dc2d24b1921
Rob Court <robbie.court@gmail.com>
2017-12-15 19:11:01 +0000

    adding library locations

--- loadScript.R
+++ loadScript.R
@@ -1,6 +1,8 @@
 #!/usr/bin/env Rscript
 
 .libPaths()
+.libPaths( c( .libPaths(), "/usr/local/lib/opencpu/site-library") )
+.libPaths( c( .libPaths(), "/usr/lib/opencpu/library") )
 options(flycircuit.datadir="/data")
 options(rgl.useNULL=TRUE)
 if (!require("Rcpp")) install.packages("Rcpp",repos="https://cloud.r-project.org/", dependencies = TRUE)

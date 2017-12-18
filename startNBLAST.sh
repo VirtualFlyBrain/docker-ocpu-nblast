890e514cbe5b967647a4e472160d6905ae012c47
Rob Court <robbie.court@gmail.com>
2017-12-18 15:34:06 +0000

    removing library clear

--- startNBLAST.sh
+++ startNBLAST.sh
@@ -17,22 +17,6 @@ chmod -R 777 /data
 chmod -R 777 /usr/local/lib/R/site-library
 chmod -R 777 /usr/lib/R/site-library
 
-rm -r /usr/lib/R/site-library/*
-
-rm -r /usr/lib/opencpu/library/brew
-rm -r /usr/lib/opencpu/library/git2r
-rm -r /usr/lib/opencpu/library/devtools
-rm -r /usr/lib/opencpu/library/jsonlite
-rm -r /usr/lib/opencpu/library/Rcpp
-rm -r /usr/lib/opencpu/library/memoise
-
-ln -s /usr/local/lib/R/site-library/brew /usr/lib/opencpu/library/brew
-ln -s /usr/local/lib/R/site-library/git2r /usr/lib/opencpu/library/git2r
-ln -s /usr/local/lib/R/site-library/devtools /usr/lib/opencpu/library/devtools
-ln -s /usr/local/lib/R/site-library/jsonlite /usr/lib/opencpu/library/jsonlite
-ln -s /usr/local/lib/R/site-library/Rcpp /usr/lib/opencpu/library/Rcpp
-ln -s /usr/local/lib/R/site-library/memoise /usr/lib/opencpu/library/memoise
-
 rm -r /usr/local/lib/R/site-library/00LOCK-*
 
 apachectl -DFOREGROUND

bdbf6183d84990a76534a495fe2c1d709952b3a7
Rob Court <robbie.court@gmail.com>
2018-03-30 15:44:29 +0100

    expanding stages

--- loadScript.R
+++ loadScript.R
@@ -10,4 +10,16 @@
 .libPaths()
 options(flycircuit.datadir="/data")
 options(rgl.useNULL=TRUE)
-if (!require("elmr")) devtools::source_gist("fdd1e5b6e009ff49e66be466a104fd92", filename = "install_flyconnectome_all.R")
+if(!require("devtools")) install.packages("devtools")
+# this will install all the regular nat packages
+devtools::source_gist("39a1182f726989db7e03", filename="install_all_nat.R")
+# elmr will bring in all the other packages we need
+devtools::install_github("jefferis/elmr", dependencies=TRUE)
+message("Setting up flycircuit dataset including pre-computed NBLAST scores")
+devtools::source_gist("bbaf5d53353b3944c090", filename = "FlyCircuitStartupNat.R")
+
+message("Downloading flycircuit neurons for de novo NBLAST - this could take 15m to overnight")
+library(flycircuit)
+dps<-read.neuronlistfh("http://flybrain.mrc-lmb.cam.ac.uk/si/nblast/flycircuit/dpscanon.rds",
+    localdir=getOption('flycircuit.datadir'))
+remotesync(dps,download.missing = TRUE)

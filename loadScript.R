10a5c0c5d488366943da3a2e76244f3116d4804c
Rob Court <rcourt@ed.ac.uk>
2018-03-30 14:41:42 +0100

    Revert "removing data dir"
    This reverts commit 5428beb87972db80f4530bd522bfd0b14a0318a1.

--- loadScript.R
+++ loadScript.R
@@ -8,5 +8,6 @@
 .libPaths( c( .libPaths(), "/usr/lib/opencpu/library") )
 .libPaths( c( .libPaths(), "/usr/lib/opencpu/site-library") )
 .libPaths()
+options(flycircuit.datadir="/data")
 options(rgl.useNULL=TRUE)
 if (!require("elmr")) devtools::source_gist("fdd1e5b6e009ff49e66be466a104fd92", filename = "install_flyconnectome_all.R")

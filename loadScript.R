73f4fe7bcd0913ff79e39f916a9b38fe1fa19727
Rob Court <rcourt@ed.ac.uk>
2021-01-26 19:20:02 +0000

    adding bit

--- loadScript.R
+++ loadScript.R
@@ -2,7 +2,7 @@
 Sys.setenv(R_REMOTES_NO_ERRORS_FROM_WARNINGS = TRUE)
 
 if (!require("devtools")) install.packages("devtools")
-
+if (!require("bit")) install.packages("bit",repos="http://cran.rstudio.com/", dependencies = FALSE, lib="/usr/local/lib/R/site-library")
 if (!require("bit64")) install.packages("bit64",repos="http://cran.rstudio.com/", dependencies = FALSE, lib="/usr/local/lib/R/site-library")
 if (!require("arrow")) { 
   install.packages("arrow",repos="http://cran.rstudio.com/", dependencies = FALSE, lib="/usr/local/lib/R/site-library")

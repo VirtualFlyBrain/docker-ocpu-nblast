26181bb33c5d66ac3ca79b1ccaeb40cefd8b1506
Rob Court <rcourt@ed.ac.uk>
2021-01-26 19:29:06 +0000

    removing lock

--- loadScript.R
+++ loadScript.R
@@ -1,13 +1,13 @@
 #!/usr/bin/env Rscript
 Sys.setenv(R_REMOTES_NO_ERRORS_FROM_WARNINGS = TRUE)
 
-if (!require("devtools")) install.packages("devtools")
-if (!require("bit")) install.packages("bit",repos="http://cran.rstudio.com/", dependencies = FALSE, lib="/usr/local/lib/R/site-library")
-if (!require("bit64")) install.packages("bit64",repos="http://cran.rstudio.com/", dependencies = FALSE, lib="/usr/local/lib/R/site-library")
+if (!require("devtools")) install.packages("devtools", INSTALL_opts = '--no-lock')
+if (!require("bit")) install.packages("bit",repos="http://cran.rstudio.com/", dependencies = FALSE, lib="/usr/local/lib/R/site-library", INSTALL_opts = '--no-lock')
+if (!require("bit64")) install.packages("bit64",repos="http://cran.rstudio.com/", dependencies = FALSE, lib="/usr/local/lib/R/site-library", INSTALL_opts = '--no-lock')
 if (!require("arrow")) { 
-  install.packages("arrow",repos="http://cran.rstudio.com/", dependencies = FALSE, lib="/usr/local/lib/R/site-library")
+  install.packages("arrow",repos="http://cran.rstudio.com/", dependencies = FALSE, lib="/usr/local/lib/R/site-library", INSTALL_opts = '--no-lock')
   library(arrow)
   install_arrow()
 }
 
-devtools::install_github("VirtualFlyBrain/vfbnblast")
+devtools::install_github("VirtualFlyBrain/vfbnblast", INSTALL_opts = '--no-lock')
